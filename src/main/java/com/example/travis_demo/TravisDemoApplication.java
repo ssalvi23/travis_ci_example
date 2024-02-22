c3beacfcc0dde9b73338d74c2266d5edce1adf9b
sakshi <sakshi@Ubuntu.myguest.virtualbox.org>
2024-02-22 21:38:30 +0530

    initial commit

---
+++ src/main/java/com/example/travis_demo/TravisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.travis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisDemoApplication.class, args);
+	}
+
+}
